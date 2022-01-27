c0f38d0ec095c5fc9a179a59d979a272ce73bc41
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2022-01-27 19:52:36 +0300

    Add files via upload

---
+++ CodeFirst_patika_dev/Models/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst_patika_dev.Entity
+{
+    public class Attendance
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+        public virtual Student Students { get; set; }
+        public float AttendanceWeek8 { get; set; }
+        [Required]
+        public int SuccessId { get; set; }
+        public virtual Success Successz { get; set; }
+
+    }
+}
---
+++ CodeFirst_patika_dev/Models/Success.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst_patika_dev.Entity
+{
+    public class Success
+    {
+        [Key]
+        public int SuccessId { get; set; }
+        [Required]
+        public float SuccessStatus { get; set; }
+        public int StudentId { get; set; }
+        public virtual Student Students { get; set; }
+
+    }
+}
