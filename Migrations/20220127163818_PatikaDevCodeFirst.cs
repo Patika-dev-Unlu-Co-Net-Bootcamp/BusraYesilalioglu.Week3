77ab5706ba07d08c096a29edb8032a937260968b
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2022-01-27 19:49:57 +0300

    Add files via upload

---
+++ Migrations/20220127163818_PatikaDevCodeFirst.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirst_patika_dev.Migrations
+{
+    public partial class PatikaDevCodeFirst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assistants",
+                columns: table => new
+                {
+                    AssistantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssistantName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssistantSurname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assistants", x => x.AssistantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentSurname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherSurname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Successz",
+                columns: table => new
+                {
+                    SuccessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SuccessStatus = table.Column<float>(type: "real", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Successz", x => x.SuccessId);
+                    table.ForeignKey(
+                        name: "FK_Successz_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Educations",
+                columns: table => new
+                {
+                    EducationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EducationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: false),
+                    AssistantId = table.Column<int>(type: "int", nullable: false),
+                    PartipantsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Educations", x => x.EducationId);
+                    table.ForeignKey(
+                        name: "FK_Educations_Assistants_AssistantId",
+                        column: x => x.AssistantId,
+                        principalTable: "Assistants",
+                        principalColumn: "AssistantId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Educations_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attendances",
+                columns: table => new
+                {
+                    AttendanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    AttendanceWeek8 = table.Column<float>(type: "real", nullable: false),
+                    SuccessId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendances", x => x.AttendanceId);
+                    table.ForeignKey(
+                        name: "FK_Attendances_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attendances_Successz_SuccessId",
+                        column: x => x.SuccessId,
+                        principalTable: "Successz",
+                        principalColumn: "SuccessId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EducationStudents",
+                columns: table => new
+                {
+                    EducationStudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    EducationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EducationStudents", x => x.EducationStudentId);
+                    table.ForeignKey(
+                        name: "FK_EducationStudents_Educations_EducationId",
+                        column: x => x.EducationId,
+                        principalTable: "Educations",
+                        principalColumn: "EducationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EducationStudents_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partipants",
+                columns: table => new
+                {
+                    PartipantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartipanttName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    APartipantSurname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EducationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partipants", x => x.PartipantId);
+                    table.ForeignKey(
+                        name: "FK_Partipants_Educations_EducationId",
+                        column: x => x.EducationId,
+                        principalTable: "Educations",
+                        principalColumn: "EducationId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_StudentId",
+                table: "Attendances",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_SuccessId",
+                table: "Attendances",
+                column: "SuccessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Educations_AssistantId",
+                table: "Educations",
+                column: "AssistantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Educations_TeacherId",
+                table: "Educations",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EducationStudents_EducationId",
+                table: "EducationStudents",
+                column: "EducationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EducationStudents_StudentId",
+                table: "EducationStudents",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partipants_EducationId",
+                table: "Partipants",
+                column: "EducationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Successz_StudentId",
+                table: "Successz",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendances");
+
+            migrationBuilder.DropTable(
+                name: "EducationStudents");
+
+            migrationBuilder.DropTable(
+                name: "Partipants");
+
+            migrationBuilder.DropTable(
+                name: "Successz");
+
+            migrationBuilder.DropTable(
+                name: "Educations");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Assistants");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
