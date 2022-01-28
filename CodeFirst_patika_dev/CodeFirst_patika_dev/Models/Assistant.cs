fd1dfda792686d48534cd4da3e342b08a838130f
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2022-01-28 15:48:12 +0300

    Add files via upload

---
+++ CodeFirst_patika_dev/CodeFirst_patika_dev/Models/Assistant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst_patika_dev.Entity
+{
+    public class Assistant 
+    {
+        [Key]
+        public int AssistantId { get; set; }
+        [Required]
+        public string AssistantName { get; set; }
+
+        public string AssistantSurname { get; set; }
+
+    }
+}
---
+++ CodeFirst_patika_dev/CodeFirst_patika_dev/Models/Education.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst_patika_dev.Entity
+{
+   
+    public class Education
+    {
+        [Key]
+        public int EducationId { get; set; }
+        [Required]
+        public string EducationName { get; set; }
+        public int TeacherId { get; set; }
+        public virtual Teacher Teachers { get; set; }
+        public int AssistantId { get; set; }
+        public virtual Assistant Assistants { get; set; }
+        public int PartipantsId { get; set; }
+        public virtual ICollection<Partipant> Partipants { get; set; }
+
+
+
+    }
+}
